00a6a92c9c21287628661620c6354eff1052d3da
vignesh <vigneshit13@gmail.com>
2019-10-13 12:57:04 +0530

    Initial commit

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return ( 
+      <div className='index-page'>
+        <h1>Story Writer</h1>
+      </div>
+     );
+  }
+}
+ 
+export default Test;
